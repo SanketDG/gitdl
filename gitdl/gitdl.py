f4cff9a311524041008d1e7a8f4ef3e43c8329a9
SanketDG <sanketdasgupta@gmail.com>
2015-08-11 00:20:40 +0530

    add docstrings and comments and remove redundant code

--- gitdl/gitdl.py
+++ gitdl/gitdl.py
@@ -4,33 +4,31 @@ import os
 import zipfile
 
 API_TOKEN = os.environ.get('GITHUB_API_TOKEN')
-params = {'API_TOKEN': API_TOKEN}
+params = {'API_TOKEN': API_TOKEN}  # create a dict to be passed by the request
 
 url = "https://api.github.com/search/repositories?q={}".format(input())
 
 
 def urlretrieve(url, path):
+    """Retrieves a zipfile and writes it to a local disk"""
     with open(path, 'wb') as f:
         r = requests.get(url, stream=True)
-        r.raise_for_status()  # Replace this with better error handling.
-
         for chunk in r.iter_content(1024):
             f.write(chunk)
 
 
 def extractfiles(zipf):
+    """Extract a zipfile to the current directory"""
     with zipfile.ZipFile(zipf, "r") as z:
         z.extractall()
 
 
 def main():
-    # request = urllib.request.Request(url)
-    # request.add_header('Authorization', 'token %s' % API_TOKEN)
     response = requests.get(url, params=params).text
     search_results = json.loads(response)
-    first_result = search_results['items'][0]
+    first_result = search_results['items'][0]  # takes out the first result
     download_url = first_result['html_url'] + '/archive/master.zip'
-    repo_name = first_result['name']
+    repo_name = first_result['name']  # stores the repository name
     print(download_url)
     urlretrieve(download_url, "{}.zip".format(repo_name))
     extractfiles("{}.zip".format(repo_name))
