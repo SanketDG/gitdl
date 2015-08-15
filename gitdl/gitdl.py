4cb1c9da0e74fe7a37919b4fd563cb95b34c250c
SanketDG <sanketdasgupta@gmail.com>
2015-08-16 05:21:34 +0530

    add proper exceptions, docstrings and refactor code

--- gitdl/gitdl.py
+++ gitdl/gitdl.py
@@ -5,10 +5,9 @@ import sys
 
 API_TOKEN = os.environ.get('GITHUB_API_TOKEN')
 
-url = "https://api.github.com/search/repositories?q={}".format(sys.argv[1])
-
 
 def get_params():
+    """Set parameters of the request"""
     if API_TOKEN is None:
         raise Exception('GITHUB_API_TOKEN not found')
     params = {'API_TOKEN': API_TOKEN}  # create a dict to be passed by request
@@ -29,16 +28,38 @@ def extractfiles(zipf):
         z.extractall()
 
 
-def main():
-    response = requests.get(url, params=get_params()).json()
-    first_result = response['items'][0]  # takes out the first result
-    download_url = first_result['html_url'] + '/archive/master.zip'
-    repo_name = first_result['name']  # stores the repository name
-    print(download_url)
-    urlretrieve(download_url, "{}.zip".format(repo_name))
+def get_first_search_result(resp):
+    "Gets first search result from json response"
+    try:
+        first_result = resp['items'][0]
+        return first_result
+    except IndexError:
+        print("No such repository exists.")
+        exit(1)
+
+
+def work_them_files(repo_name):
+    "Extract, rename and delete."
     extractfiles("{}.zip".format(repo_name))
     os.rename("{}-master".format(repo_name), "{}".format(repo_name))
     os.unlink("{}.zip".format(repo_name))
+
+
+def main():
+    # send a GET to search url in GitHub API
+    url = "https://api.github.com/search/repositories?q={}".format(sys.argv[1])
+    response = requests.get(url, params=get_params()).json()
+
+    first_result = get_first_search_result(response)  # check for empty response
+
+    download_url = first_result['html_url'] + '/archive/master.zip'
+    repo_name = first_result['name']  # stores the repository name
+    print(download_url)
+
+    urlretrieve(download_url, "{}.zip".format(repo_name))
+
+    work_them_files(repo_name)
+
 
 if __name__ == "__main__":
     main()
