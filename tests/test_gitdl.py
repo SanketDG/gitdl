0a74c45a1f4f01b53aa59f0c2cfab44c996d9020
SanketDG <sanketdasgupta@gmail.com>
2016-06-26 21:26:37 +0530

    test_gitdl: Add more tests

--- tests/test_gitdl.py
+++ tests/test_gitdl.py
@@ -61,3 +61,40 @@ class TestGitdl(unittest.TestCase):
                        headers={'Content-Length': '42'})
             r = requests.get("mock://google.com")
         self.assertEqual(gitdl.get_size(r), 0.04)
+
+    def test_get_search_results(self):
+        with requests_mock.mock() as mocker:
+            mocker.get("https://api.github.com/search/repositories?"
+                       "q=gitdl&sort=&order=desc&per_page=30",
+                       json="Found 3 repos!")
+            resp = gitdl.get_search_results("gitdl")
+        self.assertEqual(resp, "Found 3 repos!")
+
+    def test_get_first_search_result_valid(self):
+        fake_json = json.dumps({'items': [{"id": 1, "name": "gitdl"}]})
+        url = "https://api.github.com/search/repositories?q=aksejake"
+        with requests_mock.mock() as mocker:
+            mocker.get(url, json=fake_json)
+            response = json.loads(requests.get(url).json())
+        res = gitdl.get_first_search_result(response)
+        self.assertEqual(res, {'id': 1, 'name': 'gitdl'})
+
+    def test_get_repo_names(self):
+        url = "https://api.github.com/search/repositories?q=anything"
+        fake_json = json.dumps(
+            {'items': [{'id': 1, 'full_name': 'SanketDG/gitdl'},
+                       {'id': 2, 'full_name': 'SanketDG/djurl'}]})
+        with requests_mock.mock() as mocker:
+            mocker.get(url, json=fake_json)
+            response = json.loads(requests.get(url).json())
+        res = gitdl.get_repo_names(response)
+        self.assertEqual(res, ['SanketDG/gitdl', 'SanketDG/djurl'])
+
+    def test_get_search_results_first_only(self):
+        fake_json = {'items': [{"id": 1, "name": "gitdl"}]}
+        with requests_mock.mock() as mocker:
+            mocker.get("https://api.github.com/search/repositories?"
+                       "q=gitdl&sort=&order=desc&per_page=30",
+                       json=fake_json)
+            resp = gitdl.get_search_results("gitdl", only_first=True)
+        self.assertEqual(resp, {'id': 1, 'name': 'gitdl'})
